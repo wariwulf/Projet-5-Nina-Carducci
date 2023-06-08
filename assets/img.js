d1fa318dc5f2b9e06470ab7a1cee23670cf92ed5
robin.giel <robin.giel@hotmail.com>
2023-06-08 13:41:44 +0200

    Accessibilité

--- assets/img.js
+++ assets/img.js
@@ -2,39 +2,48 @@ $(document).ready(function() {
     var images = [
       {
         tag: "Concert",
-        src: "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash.webp"
+        src: "./assets/images/gallery/concerts/aaron-paul-wnX-fXzB6Cw-unsplash.webp",
+        alt: "Image de la foule d'un concert"
       },
       {
         tag: "Entreprises",
-        src: "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash.webp"
+        src: "./assets/images/gallery/entreprise/ali-morshedlou-WMD64tMfc4k-unsplash.webp",
+        alt: "Portrait d'un homme en costume d'entreprise élégant."
       },
       {
         tag: "Entreprises",
-        src: "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash.webp"
+        src: "./assets/images/gallery/entreprise/jason-goodman-tHO1_OuKbg0-unsplash.webp",
+        alt: "Image de deux collaboratrice souriante dans un bureau"
       },
       {
         tag: "Mariages",
-        src: "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash.webp"
+        src: "./assets/images/gallery/mariage/hannah-busing-RvF2R_qMpRk-unsplash.webp",
+        alt: "Deux main se tenant par les bout des doigts"
       },
       {
         tag: "Portrait",
-        src: "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash.webp"
+        src: "./assets/images/gallery/portraits/ade-tunji-rVkhWWZFAtQ-unsplash.webp",
+        alt: "Portrait d'un homme fermant les yeux"
       },
       {
         tag: "Mariages",
-        src: "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash.webp"
+        src: "./assets/images/gallery/mariage/jakob-owens-SiniLJkXhMc-unsplash.webp",
+        alt: "Deux mariés se tenant le bras et marchant de manière complice"
       },
       {
         tag: "Portrait",
-        src: "./assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash.webp"
+        src: "./assets/images/gallery/portraits/nino-van-prattenburg--443cl1uR_8-unsplash.webp",
+        alt: "Une jeune femme se tient dérrière un grillage"
       },
       {
         tag: "Concert",
-        src: "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash.webp"
+        src: "./assets/images/gallery/concerts/austin-neill-hgO1wFPXl3I-unsplash.webp",
+        alt: "Image d'un chanteur sur scène"
       },
       {
         tag: "Entreprises",
-        src: "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash.webp"
+        src: "./assets/images/gallery/entreprise/mateus-campos-felipe-Fsgzm8N0hIY-unsplash.webp",
+        alt: "Une femme d'affaire tout sourire parlant à un collègue dérrière son ordinateur"
       }
     ];
   
@@ -48,7 +57,7 @@ $(document).ready(function() {
         .attr("loading", "lazy")
         .attr("decoding", "async")
         .attr("cache-control", "max-age=604800")
-        .attr("alt", "")
+        .attr("alt", image.alt)
         .css("max-width", "100%");
       $gallery.append($img);
     });
